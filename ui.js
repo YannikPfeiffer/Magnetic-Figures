c37043810782965992128ccf1c25d78622bcd5fe
Jan Schneider <jan.schneider@adesso.de>
2019-04-06 18:17:46 +0200

    Basic UI Layout

---
+++ ui.js
@@ -0,0 +1,9 @@
+function toggleOptions(){
+    $('.ui-options').toggle();
+    $('.ui-icon-open-options').toggle();
+}
+
+function toggleKeyMap(){
+    $('.ui-keymap').toggle();
+    $('.ui-icon-open-keymap').toggle();
+}
