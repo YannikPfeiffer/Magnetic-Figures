148eb87784ab3e197620550a422e55762563993d
Jan Schneider <jan.schneider@adesso.de>
2019-04-06 19:46:13 +0200

    Styling UI

--- ui.js
+++ ui.js
@@ -1,9 +1,9 @@
 function toggleOptions(){
     $('.ui-options').toggle();
-    $('.ui-icon-open-options').toggle();
+    $('.ui-icon-open.left').toggle();
 }
 
 function toggleKeyMap(){
     $('.ui-keymap').toggle();
-    $('.ui-icon-open-keymap').toggle();
+    $('.ui-icon-open.right').toggle();
 }
